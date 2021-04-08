9d1228752275e14accabf3df3ae814930a26222d
pczinho <gabriel.admtk@gmail.com>
2021-04-08 15:49:50 -0300

    fix aa

--- fusion/settings.py
+++ fusion/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+    'django_adminlte',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core',
+
 ]
 
 MIDDLEWARE = [
